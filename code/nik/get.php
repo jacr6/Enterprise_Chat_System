f97375444dca442df0156b05d3137b298e49c2fe
Sri Krishna, Srinivas <ssrikrishna141194@gmail.com>
2017-07-31 04:25:08 +0200

    Add files via upload

---
+++ code/nik/get.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host ="localhost";
+$username="root";
+$password="";
+$dbname ="adrec1";
+
+ $get =new mysqli($host,$username,$password,$dbname);
+
+
+// function formatDate($date){
+
+// 	return date( 'g:i a',strtotime($date));
+// }
+
+ ?>
